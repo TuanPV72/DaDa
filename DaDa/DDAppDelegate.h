1dcdd950af7d7f3895674044b35921600a50b455
Tuan Pham <pvtuan@cmc.com.vn>
2014-08-07 21:46:13 +0700

    Initial Commit

---
+++ DaDa/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  DaDa
+//
+//  Created by MAC on 8/7/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DaDa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DaDa
+//
+//  Created by MAC on 8/7/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
